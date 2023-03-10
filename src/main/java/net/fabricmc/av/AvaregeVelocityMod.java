e93d004f4d32939e30f3b52d8c28ffc1470dd2c6
yohlimem <91895124+yohlimem@users.noreply.github.com>
2023-03-10 18:36:26 +0200

    Update AvaregeVelocityMod.java

--- src/main/java/net/fabricmc/av/AvaregeVelocityMod.java
+++ src/main/java/net/fabricmc/av/AvaregeVelocityMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.av.features.FindPlayerVelocity;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.Vec3d;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,19 +12,24 @@ public class AvaregeVelocityMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+
+	public static final class TimeScale{
+		public static final int TICKS = 1;
+		public static final int SECONDS = 20;
+		public static final int MINUTES = 1_200;
+		public static final int HOURS = 72_000;
+	}
 	public static final Logger LOGGER = LoggerFactory.getLogger("av");
 
 	@Override
 	public void onInitialize() {
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+		ClientTickEvents.START_CLIENT_TICK.register(client -> {
 			if(client == null) return;
 			PlayerEntity player = client.player;
 			if(player == null) return;
 			FindPlayerVelocity.onPlayerMove(player);
-			AvaregeVelocityMod.LOGGER.info(String.valueOf(FindPlayerVelocity.findPlayerAvaregeVelocity(5)));
-//			if(FindPlayerVelocity.playerMoving(player)){
-//				LOGGER.info(FindPlayerVelocity.findPlayerAvaregeVelocity(5).toString());
-//			}
+			Vec3d vel = FindPlayerVelocity.findPlayerAvaregeVelocity(20, TimeScale.SECONDS);
+//			AvaregeVelocityMod.LOGGER.info(vel);
 		});
 	}
 }
