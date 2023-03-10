86674d124d6fd20e67e8c348ed618536d409c748
yohlimem <91895124+yohlimem@users.noreply.github.com>
2023-03-10 18:14:41 +0200

    Update FindPlayerVelocity.java

--- src/main/java/net/fabricmc/av/features/FindPlayerVelocity.java
+++ src/main/java/net/fabricmc/av/features/FindPlayerVelocity.java
@@ -12,9 +12,11 @@ public class FindPlayerVelocity {
     public static List<Vec3d> PositionTimes = new ArrayList<>();
 
     public static void onPlayerMove(PlayerEntity player){
+//        PlayerEntity player = client.player;
         if(player == null) return;
-        PositionTimes.add(new Vec3d(player.getX(), player.getY(), player.getZ()));
+        PositionTimes.add(player.getPos());
         if(PositionTimes.size() > 20){
+//            AvaregeVelocityMod.LOGGER.info(PositionTimes.toString());
             PositionTimes.remove(0);
         }
 
@@ -34,17 +36,29 @@ public class FindPlayerVelocity {
 
                 Vec3d pos2 = PositionTimes.get(i+1);
 
+
+//                AvaregeVelocityMod.LOGGER.info("pos1: " + pos1.toString());
+//                AvaregeVelocityMod.LOGGER.info("pos2: " + pos2.toString());
+//                AvaregeVelocityMod.LOGGER.info(displacement(pos1, pos2).toString());
                 totalDisplacement = totalDisplacement.add(displacement(pos1, pos2));
-                AvaregeVelocityMod.LOGGER.info("totalDisplacement: " + totalDisplacement.toString());
+//                AvaregeVelocityMod.LOGGER.info("totalDisplacement: " + totalDisplacement.toString());
+//                AvaregeVelocityMod.LOGGER.info("i: " + i);
             }
             totalDisplacement = new Vec3d(totalDisplacement.x / howMuch, totalDisplacement.y / howMuch, totalDisplacement.z / howMuch);
-            AvaregeVelocityMod.LOGGER.info("totalDisplacement after divide: " + totalDisplacement.toString());
-            return totalDisplacement;
+//            AvaregeVelocityMod.LOGGER.info("totalDisplacement after divide: " + (totalDisplacement.multiply(20)).toString());
+            // to get to seconds we have to multiply by 20!!!!!!!!
+            return totalDisplacement.multiply(20);
         }
 
         return new Vec3d(0, 0, 0);
 
     }
+
+//    public static boolean playerMoving(PlayerEntity player){
+//        AvaregeVelocityMod.LOGGER.info(String.valueOf(player.getMovementSpeed()));
+//        return player.isM() > 0.1d;
+//
+//    }
 
     public static Vec3d displacement(Vec3d pos1, Vec3d pos2){
         return pos2.subtract(pos1);
